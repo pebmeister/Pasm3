c22596aa870386e2f6062ef8ea0192d5dcb26d04
Paul Baxter <jareth_2005@yahoo.com>
2026-08-03 11:25:17 -0400

    Add RULE_TYPE enum for operation types

---
+++ ruletype.h
@@ -0,0 +1,18 @@
+#pragma once
+
+enum class RULE_TYPE {
+    Op_Implied,
+    Op_Immediate,
+    Op_Absolute,
+    Op_ZeroPage,
+    Op_AbsoluteX,
+    Op_ZeroPageX,
+    Op_AbsoluteY,
+    Op_ZeroPageY,
+    Op_Indirect,
+    Op_IndirectX,
+    Op_IndirectY,
+    Op_Accumulator,
+    Op_Relative,
+    Op_ZeroPageRelative
+};
