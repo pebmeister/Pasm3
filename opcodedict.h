144ef9ee4d8294949d6b2a2202b2f955f00d6658
Paul Baxter <jareth_2005@yahoo.com>
2026-08-03 11:14:40 -0400

    Add OpCodeInfo struct and opcode dictionary
    Define OpCodeInfo structure and opcode dictionary.

---
+++ opcodedict.h
@@ -0,0 +1,35 @@
+// written by Paul Baxter
+#pragma once
+#include <cstdint>
+#include <map>
+#include <string>
+#include <string_view>
+
+#include "ruletype.h"
+#include "tokenkind.h"
+
+struct OpCodeInfo {
+    std::string_view mnemonic;
+    std::map<RULE_TYPE, std::pair<uint8_t, int>> mode_to_opcode;
+    bool is_65c02 = false;
+    bool is_illegal = false;
+    std::string_view description;
+
+    // Constructor for convenience
+    OpCodeInfo(
+        std::string_view mnemonic,
+        std::map<RULE_TYPE, std::pair<uint8_t, int>> mode_to_opcode,
+        bool is_65c02 = false,
+        bool is_illegal = false,
+        std::string_view description = ""
+    )
+        : mnemonic(std::move(mnemonic)),
+        mode_to_opcode(std::move(mode_to_opcode)),
+        is_65c02(is_65c02),
+        is_illegal(is_illegal),
+        description(std::move(description))
+    {
+    }
+};
+
+extern std::map<TokenKind, OpCodeInfo> opcodeDict;
