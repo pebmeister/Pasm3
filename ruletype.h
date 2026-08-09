20208318223a549aca9c16ae96b425cd78eea8cb
Paul Baxter <jareth_2005@yahoo.com>
2026-08-09 06:16:34 -0400

    Update ruletype.h

--- ruletype.h
+++ ruletype.h
@@ -17,4 +17,23 @@ enum RULE_TYPE {
     Op_ZeroPageRelative
 };
 
-extern map<RULE_TYPE, std:string_view> rulemap;
+extern std::map<RULE_TYPE, std::string_view> rulemap;
+
+#ifdef GEN_RULEMAP
+std::map<RULE_TYPE, std::string> rulemap {
+    { Op_Implied, "Op_Implied" },
+    { Op_Immediate, "Op_Immediate" },
+    { Op_Absolute, "Op_Absolute" },
+    { Op_ZeroPage, "Op_ZeroPage" },
+    { Op_AbsoluteX, "Op_AbsoluteX" },
+    { Op_ZeroPageX, "Op_ZeroPageX" },
+    { Op_AbsoluteY, "Op_AbsoluteY" },
+    { Op_ZeroPageY, "Op_ZeroPage" },
+    { Op_Indirect, "Op_Indirect" },
+    { Op_IndirectX, "Op_IndirectX" },
+    { Op_IndirectY, "Op_IndirectY" },
+    { Op_Accumulator, "Op_Accumulator" },
+    { Op_Relative, "Op_Relative" },
+    { Op_ZeroPageRelative, "Op_ZeroPageRelative" }
+};
+#endif
