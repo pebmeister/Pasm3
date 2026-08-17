b05458337a024074dcec911641f8dd598dfc6adb
Paul Baxter <jareth_2005@yahoo.com>
2026-08-17 05:38:24 -0400

    Use char instead of int for anon lable

--- main.cpp
+++ main.cpp
@@ -89,7 +89,7 @@ struct MacroDef {
 };
 
 struct AnonymousLabel {
-    int type;             // '-' or '+'
+    char type;             // '-' or '+'
     uint16_t address;      // PC address in memory
     std::pair<int, size_t> statement_id;   // Sequential statement/AST index for relative position
 };
@@ -182,7 +182,6 @@ struct UnaryExpr : ExprNode {
 	UnaryExpr(int op, std::unique_ptr<ExprNode> rhs)
 		: op(op), operand(std::move(rhs)) {}
 };
-
 
 struct BinaryExpr : ExprNode {
 	int op;
