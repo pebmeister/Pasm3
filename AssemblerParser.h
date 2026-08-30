0c7a80e4932fd8d417219ad0104941b785d88fb9
Paul Baxter <jareth_2005@yahoo.com>
2026-08-30 18:42:52 -0400

    Update AssemblerParser.h

--- AssemblerParser.h
+++ AssemblerParser.h
@@ -206,7 +206,7 @@ public:
                 }
                 else if (dir == ".basic_hdr") {
                     auto org_addr = std::make_unique<NumberExpr>(static_cast<uint16_t>(0x0801)));
-                    statements.push_back(std::make_unique<OrgStatement>(dir_tok.file, dir_tok.line, org_addr.release()));
+                    statements.push_back(std::make_unique<OrgStatement>(dir_tok.file, dir_tok.line, org_addr));
                     
                     std::vector<std::unique_ptr<ExprNode>> elems;
                     for (auto by: {
