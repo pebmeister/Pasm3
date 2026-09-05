93414a827dd87d01726bb233ec20a90054ba37e9
Paul Baxter <jareth_2005@yahoo.com>
2026-09-05 09:11:57 -0400

    Update multipassassembler.cpp

--- multipassassembler.cpp
+++ multipassassembler.cpp
@@ -60,7 +60,7 @@ void MultiPassAssembler::Assemble(std::vector<std::unique_ptr<Statement>>& state
         std::cout << "Pass " << pass << " complete. "
             << (changed ? "Symbols modified (needs another pass)." : "Symbols stable.") << "\n";
         pass++;
-        if (wait_clean && clean) {
+        if (wait_clean && clean && !changed) {
             wait_clean = false;
         }
     }
