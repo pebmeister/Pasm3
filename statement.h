761cdcc44a124a142dcb1241bc1c9ddfa74cb606
Paul Baxter <jareth_2005@yahoo.com>
2026-09-06 10:52:11 -0400

    Add StmtType

--- statement.h
+++ statement.h
@@ -1,5 +1,24 @@
 #pragma once 
 
+enum class StmtType {
+    Label,
+    Org,
+    Equ,
+    Ds,
+    Data,
+    Fill,
+    Instruction,
+    Print,
+    Unknown
+};
+
+enum PrintCmd {
+	on,
+	off,
+	push,
+	pop
+};
+
 #include "exprNode.h"
 
 // ============================================================================
@@ -8,19 +27,20 @@
 
 struct Statement {
     virtual ~Statement() = default;
-
+|
     int file{0};
     int line{0};
+    StmtType stmt_type{StmtType::Unknown};
 
-    Statement(int file, int line)
-        : file(file), line(line) {}
+    Statement(int file, int line, StmtType stmt_type)
+        : file(file), line(line), stmt_type(stmt_type)
 };
 
 struct LabelStatement : Statement {
     std::string name;
 	bool is_local() const { return name[0] == '@'; }
 	bool is_anon() const { return name[0] == '-' || name[0] == '+'; }
-    explicit LabelStatement(int file, int line, std::string name) : Statement(file, line), name(std::move(name)) {}
+    explicit LabelStatement(int file, int line, std::string name) : Statement(file, line, StmtType::Label), name(std::move(name)) {}
 };
 
 struct InstructionStatement : Statement {
@@ -31,24 +51,24 @@ struct InstructionStatement : Statement {
     std::vector<uint8_t> bytes;
 
     InstructionStatement(int file, int line, std::string m, RULE_TYPE mode, std::unique_ptr<ExprNode> op)
-        : Statement(file, line), mnemonic(std::move(m)), mode(mode), operand(std::move(op)) {}
+        : Statement(file, line, StmtType::Instruction), mnemonic(std::move(m)), mode(mode), operand(std::move(op)) {}
 };
 
 struct OrgStatement : Statement {
     std::unique_ptr<ExprNode> address_expr;
-    explicit OrgStatement(int file, int line, std::unique_ptr<ExprNode> expr) : Statement(file, line), address_expr(std::move(expr)) {}
+    explicit OrgStatement(int file, int line, std::unique_ptr<ExprNode> expr) : Statement(file, line, StmtType::Org), address_expr(std::move(expr)) {}
 };
 
 struct FillStatement : Statement {
     std::unique_ptr<ExprNode> byte_expr;
     std::unique_ptr<ExprNode> length_expr;
-    explicit FillStatement(int file, int line, std::unique_ptr<ExprNode> byteexpr, std::unique_ptr<ExprNode> lenexpr) : Statement(file, line),
+    explicit FillStatement(int file, int line, std::unique_ptr<ExprNode> byteexpr, std::unique_ptr<ExprNode> lenexpr) : Statement(file, line, StmtType::Fill),
         byte_expr(std::move(byteexpr)), length_expr(std::move(lenexpr)) {}
 };
 
 struct DsStatement : Statement {
     std::unique_ptr<ExprNode> size_expr;
-    explicit DsStatement(int file, int line, std::unique_ptr<ExprNode> expr) : Statement(file, line), size_expr(std::move(expr)) {}
+    explicit DsStatement(int file, int line, std::unique_ptr<ExprNode> expr) : Statement(file, line, StmtType::Ds), size_expr(std::move(expr)) {}
 };
 
 struct EquStatement : Statement {
@@ -57,7 +77,7 @@ struct EquStatement : Statement {
 
     std::unique_ptr<ExprNode> value_expr;
     EquStatement(int file, int line, std::string n, std::unique_ptr<ExprNode> expr)
-        : Statement(file, line), name(std::move(n)), value_expr(std::move(expr)) {}
+        : Statement(file, line, StmtType::Equ), name(std::move(n)), value_expr(std::move(expr)) {}
 };
 
 enum class DataWidth { Byte, Word };
@@ -67,17 +87,10 @@ struct DataStatement : Statement {
     std::vector<std::unique_ptr<ExprNode>> elements;
 
     DataStatement(int file, int line, DataWidth w, std::vector<std::unique_ptr<ExprNode>> elems)
-        : Statement(file, line), width(w), elements(std::move(elems)) {}
-};
-
-enum PrintCmd {
-	on,
-	off,
-	push,
-	pop
+        : Statement(file, line, StmtType::Data), width(w), elements(std::move(elems)) {}
 };
 
 struct PrintStatement : Statement {
     PrintCmd cmd;
-    explicit PrintStatement(int file, int line, PrintCmd cmd) : Statement(file, line), cmd(std::move(cmd)) {}
+    explicit PrintStatement(int file, int line, PrintCmd cmd) : Statement(file, line, StmtType::Print), cmd(std::move(cmd)) {}
 };
